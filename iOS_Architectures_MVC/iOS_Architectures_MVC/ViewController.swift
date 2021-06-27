4445b5e684b65f9b974a106280ab1a96aebc5108
Pete Smith <peadar81@gmail.com>
2021-06-27 11:32:54 +0100

    Create workspace, initial MVC project

---
+++ iOS_Architectures_MVC/iOS_Architectures_MVC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS_Architectures_MVC
+//
+//  Created by Pete Smith on 27/06/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
